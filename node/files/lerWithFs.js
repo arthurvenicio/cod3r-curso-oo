be4dfceea89a8b4ceeb14033a3040e9764c7a290
arthurvenicio <stuks.midia@gmail.com>
2021-10-25 02:45:47 -0300

    Aula 17

---
+++ node/files/lerWithFs.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+
+const caminho = __dirname + "/config.json";
+
+const conteudo = fs.readFileSync(caminho, "utf-8");
+// console.log(conteudo);
+
+fs.readFile(caminho, "utf-8", (err, conteudo) => {
+  const config = JSON.parse(conteudo);
+  console.log(`${config.db.host}:${config.db.port}`);
+});
+
+const config = require("./config.json");
+console.log(config.db);
