5d8a9a672ab1d3f276e4e56952597fe9db3fcf29
arthurvenicio <stuks.midia@gmail.com>
2021-10-25 00:10:49 -0300

    Aula 14.1

--- node/index.js
+++ node/index.js
@@ -2,15 +2,15 @@ const axios = require("axios");
 
 const url = "http://files.cod3r.com.br/curso-js/funcionarios.json";
 
-const isChinese = (f) => f.pais === "China";
+const isChinese = (f) => f.pais === "Brazil";
 
-const isWoman = (f) => f.genero === "F";
+const isWoman = (f) => f.genero === "M";
 
 const smallSalary = (f, Acf) => {
   return f < Acf ? f : Acf;
 };
 
-const data = axios.get(url).then((response) => {
+axios.get(url).then((response) => {
   const data = response.data;
   const womanChinese = data
     .filter(isChinese)
@@ -18,5 +18,3 @@ const data = axios.get(url).then((response) => {
     .reduce(smallSalary);
   console.log(womanChinese);
 });
-
-const woman = data;
