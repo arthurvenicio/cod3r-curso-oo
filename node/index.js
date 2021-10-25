501fb3f61d05dd0c8a22bba74892f9701d5a590a
arthurvenicio <stuks.midia@gmail.com>
2021-10-25 00:08:28 -0300

    Aula 14

--- node/index.js
+++ node/index.js
@@ -1,3 +1,22 @@
-const exporta = require("./modules/exportar");
+const axios = require("axios");
 
-console.log(exporta);
+const url = "http://files.cod3r.com.br/curso-js/funcionarios.json";
+
+const isChinese = (f) => f.pais === "China";
+
+const isWoman = (f) => f.genero === "F";
+
+const smallSalary = (f, Acf) => {
+  return f < Acf ? f : Acf;
+};
+
+const data = axios.get(url).then((response) => {
+  const data = response.data;
+  const womanChinese = data
+    .filter(isChinese)
+    .filter(isWoman)
+    .reduce(smallSalary);
+  console.log(womanChinese);
+});
+
+const woman = data;
