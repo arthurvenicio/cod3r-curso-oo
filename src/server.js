e67e8f3e57ef74d4548439006b417849dd6473ee
arthurvenicio <stuks.midia@gmail.com>
2021-10-26 00:59:06 -0300

    Aula 22

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const bd = require("./bd");
 const Product = require("./models/product.model");
+
 const app = express();
 
 app.use(express.json());
@@ -9,7 +11,6 @@ app.use(
   })
 );
 
-const bd = require("./bd");
 const result = {};
 
 const getConsole = (req, res, next) => {
